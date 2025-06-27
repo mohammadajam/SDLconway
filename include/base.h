8ac4bbbaca26fd795ab4f3295c4d965f4db06065
Mohammad Ajam <mohammad.wael.s.ajam@gmail.com>
2025-06-27 06:12:43 +0300

    Add a base file with basic functionalities

---
+++ include/base.h
@@ -0,0 +1,38 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <stdint.h>
+#include <time.h>
+
+// defining type aliases
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef uint64_t u64;
+
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
+
+typedef float f32;
+typedef double f64;
+
+/*
+ * defining a logging macro that logs a string when called in debug. 
+ * otherwise it does nothing in release mode when compiled using the cmake file
+ */
+#ifdef NDEBUG
+#define LOG(x)
+#else
+#define LOG(x) { \
+    time_t current_time = time(NULL); \
+    char* stime = ctime(&current_time); \
+    if (stime) { \
+        stime[strlen(stime) - 1] = '\0'; /* remove \n */ \
+        printf("%s | %s\n", stime, x); \
+    } \
+}
+#endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,13 +1,13 @@
 #define SDL_MAIN_USE_CALLBACKS 1
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
+#include "base.h"
 
 static SDL_Window* window = NULL;
 static SDL_Renderer* renderer = NULL;
 
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]) {
 	SDL_SetAppMetadata("SDL Conway", "1.0", "com.conway.game");
-
 
 	if (!SDL_Init(SDL_INIT_VIDEO)) {
 		SDL_Log("Couldn't Initialize Window %s", SDL_GetError());
